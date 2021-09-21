3f54717ea8d3ad66ac607bacf58350ff901321eb
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-09-21 09:48:19 +0000

    main welcome selection for new or existing function added

--- run.py
+++ run.py
@@ -12,8 +12,19 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('retro-bank')
 
-users = SHEET.worksheet('customer-data')
-
-data = users.get_all_values()
-
-print(data)
+def welcome():
+   while True:
+        try:
+            choice = input("If you are a new customer please enter 1:\n" +
+                           "If you are already are an existing customer please enter 2:\n")
+            if choice != "1" and choice != "2":
+                raise ValueError(
+                    "Enter 1 for new customer or 2 for existing customer," +
+                    f"you entered: {choice}"
+                )
+            else:
+                return choice
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again.\n")
+            
+welcome()
