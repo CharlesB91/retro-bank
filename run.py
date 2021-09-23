0a230510298b4f81dcadd59b69a9d4f28ebbdd1f
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-09-23 11:25:37 +0000

    withdrawl func removed as too messy. Need to research for simple solution

--- run.py
+++ run.py
@@ -61,6 +61,7 @@ def regDetails():
     sheet = SHEET.worksheet("customer-data")
     new_cust = []
     bonus = 500
+    
 
     while True:
         name = input("Please enter your full name: ")
@@ -179,48 +180,6 @@ def balance():
                 exit()
         except ValueError as e:
             print(f"Invalid data: {e}, please try again.\n")
-
-def withdrawl():
-    user = SHEET.worksheet(ename)
-    balance = user.col_values(4)
-    value = balance[0]
-    print(f"The balance of your account is {value}")
-    
-    while True:
-        try:
-            choice = input("How much would you like to withdraw ? ")
-            choice = int(choice)
-            if choice != int(choice):
-                raise ValueError(
-                    "Please enter a valid amount" +
-                    f"you entered: {choice}"
-                )
-            else:
-                print("Done")
-        except ValueError as e:
-            print(f"Invalid data: {e}, please try again.\n")
-
-# def deposit():
-#     user = SHEET.worksheet(ename)
-#     balance = user.col_values(4)
-#     value = balance[0]
-#     print(f"The balance of your account is {value}")
-    
-#     while True:
-#         try:
-#             choice = input("If you would like to complete an other transctions please 1 for main menu " +
-#                            "If you would like to log out its 2:\n")
-#             if choice != "1" and choice != "2":
-#                 raise ValueError(
-#                     "Enter 1 for new customer or 2 for selection" +
-#                     f"you entered: {choice}"
-#                 )
-#             elif choice == "1":
-#                 mainMenu()
-#             else:
-#                 exit()
-#         except ValueError as e:
-#             print(f"Invalid data: {e}, please try again.\n")
 
 
 
